ce90037a07a3102d64f4459ce987c9fdeb809204
Siddharth <officialsiddharthbisht@gmail.com>
2022-09-19 23:28:05 +0530

    CORS Policy error resolved from backend

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-cors({
-    // origin : "http://127.0.0.1:3000",
-    origin : "*",
-})
+app.use(cors(
+    {
+        "origin": "*",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+      }
+));
 
 app.use(express.json());
 // route imports
